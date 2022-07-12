ad075a6aa6cde667f741520806ed1fd7ce2baebb
Asanka Nissanka <asanka@getshoutout.com>
2022-07-12 15:26:05 +0800

    updated root module

--- main.tf
+++ main.tf
@@ -1,11 +1,27 @@
+terraform {
+  cloud {
+    organization = "Moosarc"
+    workspaces {
+      name = "learn_private_module_root"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0.0"
+    }
+  }
+}
+
 provider "aws" {
   region = var.region
 }
 
-module "s3-webapp" {
-  source  = "app.terraform.io/hashicorp-learn/s3-webapp/aws"
-  name        = var.name
-  region = var.region
-  prefix = var.prefix
-  version = "1.0.0"
+module "s3-static-cdn" {
+  source  = "app.terraform.io/Moosarc/s3-static-cdn/aws"
+  name    = var.name
+  region  = var.region
+  prefix  = var.prefix
+  version = "0.9.0"
 }
